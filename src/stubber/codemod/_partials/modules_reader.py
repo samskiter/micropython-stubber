3b4482c305df0cb0a20084ed1a9669d63c1f05a6
Jos Verlinde <Jos.Verlinde@Microsoft.com>
2023-03-07 20:32:40 +0100

    try to read modulelist from multiple folders
    Signed-off-by: Jos Verlinde <Jos.Verlinde@Microsoft.com>

--- src/stubber/codemod/_partials/modules_reader.py
+++ src/stubber/codemod/_partials/modules_reader.py
@@ -1,13 +1,56 @@
 """partial used to read the modulelist.txt file"""
-###PARTIAL###
+from typing import TYPE_CHECKING, List, type_check_only
+
+if TYPE_CHECKING:
+    import sys
+    import logging
+
+    @type_check_only
+    class Stubber:
+        path: str
+        _report: List[str]
+        modules = []
+
+        def __init__(self, path: str = "", firmware_id: str = "") -> None:
+            ...
+
+        def clean(self) -> None:
+            ...
+
+        def create_one_stub(self, modulename: str) -> bool:
+            ...
+
+        def report(self, filename: str = "modules.json"):
+            ...
+
+        def create_all_stubs(self):
+            ...
+
+    @type_check_only
+    def read_path() -> str:
+        ...
+
+    @type_check_only
+    class _gc:
+        def collect(self) -> None:
+            ...
+
+    gc: _gc
+    _log = logging.getLogger("stubber")
+
+    # help type checker
+    stubber = Stubber()
+
+    ###PARTIAL###
 # Read stubs from modulelist in the current folder or in /libs
 # fall back to default modules
 stubber.modules = ["micropython"]
-for p in ["", "/lib"]:
+for p in ["", "/", "/lib/"]:
     try:
         with open(p + "modulelist" + ".txt") as f:
-            # not optimal , but works on mpremote and eps8266
+            # not optimal, but works on mpremote and eps8266
             stubber.modules = [l.strip() for l in f.read().split("\n") if len(l.strip()) and l.strip()[0] != "#"]
+            _log.info("Using %smodulelist.txt", p)
             break
     except OSError:
         pass
